132e199afe5d959bcd3609b465319e3cd2fa62ad
SyedGuo <syed.guo@tbwa.com>
2022-01-28 17:55:22 +0800

    problem

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,14 +3,15 @@ class EventsController < ApplicationController
     before_action :set_event, :only => [ :show, :edit, :update, :destory]
 
     def index
-        @events = Event.all
-    end
+        @events = Event.order(:id).page(params[:page]).per(5)
+      end
 
     def new
         @event = Event.new
     end
 
     def create
+        flash[:notice] = "event was successfully created"
         @event = Event.new(event_params)
         if @event.save
             redirect_to :action => :index
@@ -27,12 +28,13 @@ class EventsController < ApplicationController
     end
 
     def update
+        flash[:notice] = "#{@event.name} was successfully updated"
         @event.update(event_params)
-
         redirect_to :action => :show, :id =>@event 
     end
 
     def destory
+        flash[:alert] = "#{@event.name} was successfully deleted" 
         @event.destroy
 
         redirect_to :action => :index
